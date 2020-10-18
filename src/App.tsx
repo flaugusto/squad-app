6364ceeb9602ee85b51a3d48ac0c29f32033d213
Flavio Augusto <augustof.cunha@outlook.com>
2020-10-17 22:53:48 -0300

    Initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,36 @@ import React from 'react';
 
 import Home from './pages/Home';
 
+import logo from './assets/images/logo.png';
+
 import './assets/styles/global.css';
+import './App.css';
 
 function App() {
 	return (
-		<div className="app-content">
-			<Home></Home>
-		</div>
+        <div id="app">
+            <header>
+                <div id="app-info">
+                    <img 
+                        className="venturus-logo" 
+                        src={logo} 
+                        alt="Venturus Logo"/>
+                    <h3 className="app-title"> Squad Management Tool</h3>
+                </div>
+                <div id="user-info">
+                    <h4><strong>Flavio Augusto</strong></h4>
+                    <img 
+                        className="user-avatar"
+                        src="https://avatars1.githubusercontent.com/u/35996925?s=460&u=1930c3dc11fc29d82cdd77b153b378ff904025a5&v=4" 
+                        alt="User avatar icon."
+                    />
+                </div>
+            </header>
+            <Home></Home>
+            <footer>
+                <span>2020 - All rights reserved</span>
+            </footer>
+        </div>
 	);
 }
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,52 +1,31 @@
 import React from 'react';
 
 import './styles.css';
-import logo from '../../assets/images/logo.png';
 
 function Home() {
     return (
-        <div>
-            <header>
-                <div id="app-info">
-                    <img src={logo} alt="Venturus Logo"/>
-                    <h2><strong>Squad Management Tool</strong></h2>
-                </div>
-                <div id="user-info">
-                    <h4><strong>Flavio Augusto</strong></h4>
-                    <img 
-                        src="https://avatars1.githubusercontent.com/u/35996925?s=460&u=1930c3dc11fc29d82cdd77b153b378ff904025a5&v=4" 
-                        alt="User avatar icon."
-                    />
-                </div>
-            </header>
-            
-            <div className="box">
-                <div className="grid-container">
-                    <div className="card my-teams">
-                        <h1 className="card-header">My Teams</h1>
-                        <div className="card-body">
-                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        </div>
-                    </div>
-                    <div className="card top-5">
-                        <h1 className="card-header">Top 5</h1>
-                        <div className="card-body">
-                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        </div>
-                    </div>
-                    <div className="card most-picked">
-                        <h5 className="card-header">Special title treatment</h5>
-                        <div className="card-body">
-                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        </div>
+        <section>
+            <div className="grid-container">
+                <div className="card my-teams">
+                    <h1 className="card-header">My Teams</h1>
+                    <div className="card-body">
+                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
                     </div>
                 </div>
-
-                <footer>
-                    <span>2020 - All rights reserved</span>
-                </footer>
+                <div className="card top-5">
+                    <h1 className="card-header">Top 5</h1>
+                    <div className="card-body">
+                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+                <div className="card most-picked">
+                    <h5 className="card-header">Special title treatment</h5>
+                    <div className="card-body">
+                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
             </div>
-        </div>
+        </section>
     )
 }
 
