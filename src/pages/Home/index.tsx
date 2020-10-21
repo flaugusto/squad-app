ed83461bc0097cebafcb460f49cafd738b2ca9b6
Flavio Augusto <augustof.cunha@outlook.com>
2020-10-21 00:17:22 -0300

    Creating team and styles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPen, faTrash, faShareAlt } from '@fortawesome/free-solid-svg-icons'
+
 
 import './styles.css';
 
@@ -7,19 +10,39 @@ function Home() {
         <section>
             <div className="grid-container">
                 <div id="my-teams" className="card">
-                    <h1 className="card-header">My Teams</h1>
-                    <div className="card-body">
-                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <h1 className="header">My Teams</h1>
+                    <div className="body">
+                        <table className="team">
+                            <tr>
+                                <td><span id="name">Barcelona</span></td>
+                                <td>
+                                    <div id="description">
+                                        <span>Barcelona Squad</span>
+                                        <div className="buttons-container">
+                                            <a href="#" className="delete">
+                                                <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+                                            </a>
+                                            <a href="#" className="share">
+                                                <FontAwesomeIcon icon={faShareAlt}></FontAwesomeIcon>
+                                            </a>
+                                            <a href="#" className="edit">
+                                                <FontAwesomeIcon icon={faPen}></FontAwesomeIcon>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>
                     </div>
                 </div>
                 <div id="top-5" className="card">
-                    <h1 className="card-header">Top 5</h1>
-                    <div className="card-body">
-                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <h1 className="header">Top 5</h1>
+                    <div className="body">
+                        <p className="text">With supporting text below as a natural lead-in to additional content.</p>
                     </div>
                 </div>
                 <div id="most-picked" className="card">
-                    <div className="card-body">
+                    <div className="body">
                     </div>
                 </div>
             </div>
